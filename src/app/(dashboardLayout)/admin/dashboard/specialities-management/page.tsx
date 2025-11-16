d56480ab77f4c37e2a5fc28a6b6bbf3f7e2a39c2
Ayub40 <ayubk4028@gmail.com>
2025-11-17 01:35:31 +0600

    doctor Management and doctors validation added

--- src/app/(dashboardLayout)/admin/dashboard/specialities-management/page.tsx
+++ src/app/(dashboardLayout)/admin/dashboard/specialities-management/page.tsx
@@ -1,18 +1,25 @@
 import SpecialitiesManagementHeader from "@/components/modules/Admin/SpecialitiesManagement/SpecialitiesManagementHeader";
+import SpecialitiesTable from "@/components/modules/Admin/SpecialitiesManagement/SpecialitiesTable";
 import RefreshButton from "@/components/shared/RefreshButton";
+import { TableSkeleton } from "@/components/shared/TableSkeleton";
+import { getSpecialities } from "@/services/admin/specialitiesManagement";
+import { Suspense } from "react";
 
-const AdminSchedulesManagementPage = () => {
+const AdminSpecialitiesManagementPage = async () => {
+    const result = await getSpecialities();
+    console.log(result);
+
     return (
         <div className="space-y-6">
             <SpecialitiesManagementHeader />
             <div className="flex">
                 <RefreshButton />
             </div>
-            {/* <Suspense fallback={<TableSkeleton columns={2} rows={10} />}>
+            <Suspense fallback={<TableSkeleton columns={2} rows={10} />}>
                 <SpecialitiesTable specialities={result.data} />
-            </Suspense> */}
+            </Suspense>
         </div>
     );
 };
 
-export default AdminSchedulesManagementPage;
+export default AdminSpecialitiesManagementPage;
