a643eea6cf154b0bfeea9e20491da585ef7bbcdf
Ayub40 <ayubk4028@gmail.com>
2025-12-01 00:09:56 +0600

    ChangeAppointmentStatusDialog.tsx added

---
+++ src/app/(dashboardLayout)/doctor/dashboard/appointments/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+    return (
+        <div>
+            <h1 className="text-4xl font-bold">This is Doctor Appointment page Component</h1>
+        </div>
+    );
+}
