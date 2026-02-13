d5623e2e816d5a8f9446f64d8bef90807645480d
Ayub40 <ayubk4028@gmail.com>
2026-02-14 01:31:26 +0600

    root page.tsx updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
+import LoginSuccessToast from "@/components/shared/LoginSuccessToast";
+import LogoutSuccessToast from "@/components/shared/LogoutSuccessToast";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Suspense } from "react";
 import { Toaster } from "sonner";
-import LogoutSuccessToast from "@/components/shared/LogoutSuccessToast";
-import LoginSuccessToast from "@/components/shared/LoginSuccessToast";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,7 +17,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Health-Care",
+  title: "PH-Health-Care",
   description: "A healthcare application built with Next.js",
 };
 
@@ -31,9 +32,11 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Toaster position="top-right" richColors />
-        <LoginSuccessToast />
-        <LogoutSuccessToast />
+        <Toaster position="bottom-right" richColors />
+        <Suspense fallback={null}>
+          <LoginSuccessToast />
+          <LogoutSuccessToast />
+        </Suspense>
       </body>
     </html>
   );
